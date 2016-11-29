7af0881690c1821f07b31a6cce8b2dff012790c3
Nicky Somers <nickysomers@MacBook-Pro-van-Nicky.local>
2016-11-29 17:24:59 +0100

    Fixed some little things

--- wp-content/themes/Designosource/404.php
+++ wp-content/themes/Designosource/404.php
@@ -1,18 +1,18 @@
-<?php
-    get_header();
-?>
+<?php get_header(); ?>
 
 <div class="not-found">
+
 	<div class="not-found__content">
 		<h2 class="not-found__content__subtitle">Pagina niet gevonden</h2>
-		<h1>Oeps! Er heeft iemand een foutje gemaakt.</h1>
+		<h1 class="not-found__content__title">Oeps! Er heeft iemand een foutje gemaakt.</h1>
 		
 		<a href="<?php echo site_url(); ?>/team" class="button">Team</a>
 		<a href="<?php echo site_url(); ?>/projecten" class="button">Projecten</a>
 		
 	</div>
 	
-	
 </div>
+
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
 <?php get_footer(); ?>
